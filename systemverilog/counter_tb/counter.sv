2d4e7e71166e7ad167f9d4ba4c6e52dfcd9cd356
Yi-Chun Hsieh <114792163+Noobabc12345@users.noreply.github.com>
2025-09-12 12:11:29 -0700

    Create counter.sv

---
+++ systemverilog/counter_tb/counter.sv
@@ -0,0 +1,15 @@
+module counter #(parameter int w = 8)(
+	input logic clk,
+  input logic rst_n,
+  input logic up, // 1=up, 0=down
+  input logic enable,
+  output logic [w-1:0] cnt
+);
+  always @(posedge clk) begin
+    if (!rst_n)
+      cnt <='0;
+    end else if (en) begin
+      cnt <= up ? (cnt + 1'b1) : (cnt - 1'b1);
+    end
+  end
+endmodule
