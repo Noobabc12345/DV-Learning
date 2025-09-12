d8a4dcb69bebe92b77f7141729622ccbfbc0d0d0
Yi-Chun Hsieh <114792163+Noobabc12345@users.noreply.github.com>
2025-09-12 12:14:17 -0700

    Update counter.sv

--- systemverilog/counter_tb/counter.sv
+++ systemverilog/counter_tb/counter.sv
@@ -2,13 +2,13 @@ module counter #(parameter int w = 8)(
 	input logic clk,
   input logic rst_n,
   input logic up, // 1=up, 0=down
-  input logic enable,
+  input logic en,
   output logic [w-1:0] cnt
 );
   always @(posedge clk) begin
     if (!rst_n)
       cnt <='0;
-  end else if (enable) begin
+  end else if (en) begin
       cnt <= up ? (cnt + 1'b1) : (cnt - 1'b1);
     end
   end
