876e049f118a40a19f9a0a6518f61d2da4a59e06
Yi-Chun Hsieh <114792163+Noobabc12345@users.noreply.github.com>
2025-09-26 22:32:59 -0700

    Update alu_tb.sv

--- systemverilog/alu_tb/alu_tb.sv
+++ systemverilog/alu_tb/alu_tb.sv
@@ -1,5 +1,18 @@
 // alu testbench
-module alu #(
-  parameter int W = 4
-)
-  input
+module alu_tb;
+  localparam int W = 4;
+
+  logic [W-1:0] a, b, y;
+  logic zero, carry;
+  logic [2:0] op;
+
+  alu #(W) uut(.a(a), .b(b), .y(y), .zero(zero), .carry(carry), .op(op));
+
+  integer mismatch = 0;
+    
+  initial begin
+    
+    
+
+  end
+endmodule
